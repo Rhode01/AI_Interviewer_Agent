81c9c585b37553e9a713470605e33cbd484d52c8
Rhode01 <Rhodrickmhone@gmail.com>
2025-08-26 17:28:44 +0200

    .

---
+++ src/data/db/core/config.py
@@ -0,0 +1,30 @@
+from typing import Any, Union, Optional
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import PostgresDsn, field_validator
+from pathlib import Path
+ENV_PATH =(Path(__file__).resolve().parents[1] / ".env")
+class Settings(BaseSettings):
+    POSTGRES_HOST: str
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+    POSTGRES_PORT: str = "5432"
+    
+    SQLALCHEMY_DATABASE_URI: Union[PostgresDsn, None] = None
+
+    model_config = SettingsConfigDict(env_file=ENV_PATH)
+
+    @field_validator("SQLALCHEMY_DATABASE_URI", mode="before")
+    def assemble_db_connection(cls, v: str | None, values: dict[str, Any]) -> Any:
+        if isinstance(v, str):
+            return v
+        return PostgresDsn.build(
+            scheme="postgresql+asyncpg",
+            username=values.data.get("POSTGRES_USER"),
+            password=values.data.get("POSTGRES_PASSWORD"),
+            host=values.data.get("POSTGRES_HOST"),
+            port=int(values.data.get("POSTGRES_PORT", 5432)),
+            path=values.data.get('POSTGRES_DB'),  
+        )
+
+settings = Settings()
