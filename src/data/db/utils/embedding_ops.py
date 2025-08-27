9bc7b614f2f0bffb2d49bb7a88c97e52d0c0c905
Rhode01 <Rhodrickmhone@gmail.com>
2025-08-27 15:44:31 +0200

    .

--- src/data/db/utils/embedding_ops.py
+++ src/data/db/utils/embedding_ops.py
@@ -1,9 +1,8 @@
-from sqlalchemy import select
+from sqlalchemy import select,func
 from src.data.db.models.schema import DocumentEmbedding
 from src.model.embedding import embeddings
 async def search_embeddings(session, query: str, label: str, k: int = 5):
     query_vec = embeddings.embed_query(query)
-
     stmt = (
         select(DocumentEmbedding)
         .where(DocumentEmbedding.label == label)
@@ -12,3 +11,7 @@ async def search_embeddings(session, query: str, label: str, k: int = 5):
     )
     results = await session.execute(stmt)
     return results.scalars().all()
+async def label_exists(session, label: str) -> bool:
+    stmt = select(func.count()).select_from(DocumentEmbedding).where(DocumentEmbedding.label == label)
+    result = await session.execute(stmt)
+    return result.scalar() > 0
--- src/data/load_kb.py
+++ src/data/load_kb.py
@@ -1,33 +1,53 @@
 from pathlib import Path
 import sys
-project_folder = Path(__file__).resolve().parents[2]
-sys.path.append(str(project_folder))
-from langchain_community.vectorstores import FAISS
 from langchain_community.document_loaders.text import TextLoader
 from langchain_community.document_loaders.pdf import PyPDFLoader
 from langchain_experimental.text_splitter import SemanticChunker
 from pathlib import Path
 from src.model.embedding import embeddings
+from src.data.db.models.schema import DocumentEmbedding
+from src.data.db.query.retriever import PostgresRetriever
+from src.data.db.utils.embedding_ops import label_exists
+from src.data.db.base import AsyncSessionLocal
 
-def load_kb():
-    docs_path = Path(__file__).resolve().parents[1] / "data" / "knowledge_base"
-    docs = []
+async def store_embeddings_toDb(session, docs, label: str):
+    db_objs = []
+    for doc in docs:
+        vector = embeddings.embed_query(doc.page_content)
+        db_objs.append(
+            DocumentEmbedding(
+                label=label,
+                content=doc.page_content,
+                embedding=vector
+            )
+        )
+    session.add_all(db_objs)
+    await session.commit()
 
-    for file in docs_path.glob("*.txt"):
-        loader = TextLoader(str(file))
-        docs.extend(loader.load())
-    for file in docs_path.glob("*.pdf"):
-        loader = PyPDFLoader(str(file))
-        docs.extend(loader.load())
-    chucker = SemanticChunker(
-        embeddings, 
-        breakpoint_threshold_type="percentile",
-        breakpoint_threshold_amount=95,
-        min_chunk_size=800
-    )
-    page_content = [doc.page_content for doc in docs]
-    chuck_docs = chucker.split_documents(docs)
-    vectore_store =  FAISS.from_documents(chuck_docs,embeddings)
-    retriever = vectore_store.as_retriever()
-    return retriever
-    
+
+async def load_kb(label: str):
+    async with AsyncSessionLocal() as session:
+        if await label_exists(session, label):
+            print(f"[INFO] Knowledge base for '{label}' already exists. Using existing embeddings.")
+            return PostgresRetriever(label=label, k=5)
+        docs_path = Path(__file__).resolve().parents[1] / "data" / "knowledge_base"
+        docs = []
+
+        for file in docs_path.glob("*.txt"):
+            loader = TextLoader(str(file))
+            docs.extend(loader.load())
+        for file in docs_path.glob("*.pdf"):
+            loader = PyPDFLoader(str(file))
+            docs.extend(loader.load())
+        chucker = SemanticChunker(
+            embeddings, 
+            breakpoint_threshold_type="percentile",
+            breakpoint_threshold_amount=95,
+            min_chunk_size=800
+        )
+        chuck_docs = chucker.split_documents(docs)
+        
+        await store_embeddings_toDb(session, chuck_docs, label)
+        return PostgresRetriever(label=label, k=5)
+
+
